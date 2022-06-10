47fbc848b7b130b6a7d3a2f8ed0519cba137a93f
Marcelo Bueno <marcelo.f.bueno@hotmail.com>
2022-06-10 08:52:38 -0300

    Projeto concluído

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 // Listagem das pizzas
 
@@ -21,6 +23,7 @@ pizzaJson.map((item, index)=> {
 
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -59,3 +62,127 @@ function closeModal() {
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
     item.addEventListener('click', closeModal);
 });
+
+c('.pizzaInfo--qtmenos').addEventListener('click', function(){
+    if (modalQt > 1)
+    {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', function(){
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+
+});
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+
+    let key = cart.findIndex((item)=>item.identifier == identifier);
+
+    if (key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        });
+    }
+    updateCart();
+    closeModal();
+});
+
+c('.menu-openner').addEventListener('click', () => {
+    if (cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+});
+
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw';
+});
+
+function updateCart() {
+    c('.menu-openner').innerHTML = cart.length;
+
+
+    if (cart.length > 0) {
+        c('aside').classList.add('show');
+
+        c('.cart').innerHTML = '';
+
+        let subTotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        for (let i in cart) {
+
+            let pizzaItem = pizzaJson.find((item)=>{
+                return item.id == cart[i].id;
+            });
+
+            subTotal = pizzaItem.price * cart[i].qt;
+
+            let cartItem = c('.model .cart--item').cloneNode(true);
+
+            let pizzaSizeName;
+
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--tem-nome').innerHTML = pizzaName ;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if (cart[i].qt > 1){
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
+
+            c('.cart').append(cartItem);
+
+        }
+
+        desconto = subTotal * 0.1;
+        total = subTotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subTotal.toFixed()}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed()}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed()}`;
+
+    } else {
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
+    }
+}
